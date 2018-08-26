5b42ce6c67ed18e292c372e8debb3a9aac047c83
dshtokalo <shtokalo@1cont.ru>
2018-08-26 21:07:54 +0500

    Количество перестановок с повторениями

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Test
 {
@@ -6,7 +7,32 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(NumberOfPermutations("AAABBAC"));
+        }
+
+
+        public static Int32 NumberOfPermutations(string input)
+        {
+            var charNumbers = input.GroupBy(x => x, (c, enumerable) => enumerable.Count());
+            
+            var multiplicationFactorials = 1;
+            foreach (Int32 charNumber in charNumbers)
+            {
+                multiplicationFactorials *= Factorial(charNumber);
+            }
+
+            return Factorial(input.Length) / multiplicationFactorials;
+        }
+
+        public static Int32 Factorial(Int32 n)
+        {
+            Int32 result = 1;
+            for (Int32 i = 0; i < n; i++)
+            {
+                result *= (i + 1);
+            }
+
+            return result;
         }
     }
 }
